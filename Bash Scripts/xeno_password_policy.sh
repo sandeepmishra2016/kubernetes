5127c56309631126557a59b7810b4c432fe57950
sandeepmishra2016 <sandeepmishra31jan@gmail.com>
2020-03-08 21:03:02 +0530

    removed bash folder

--- Bash Scripts/xeno_password_policy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-count=grep -i 'password for user xeno will expire in 6 days' /var/log/secure.2 | wc -l
-if [[ count -gt 0 ]] then
-#chage -I -1 -m 0 -M 99999 -E -1 xeno
-echo "this is test email " | mailx -s 'xeno_password_policy cron job' sandeepmishra@fico.com siddiquekhan@fico.com
-fi
